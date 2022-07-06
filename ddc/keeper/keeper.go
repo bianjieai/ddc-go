00c1a48467ce7d5e48507ff48ac486641889a4f0
Dreamer <745124335@qq.com>
2022-07-06 16:33:06 +0800

    ddc keeper implement Hook

--- ddc/keeper/keeper.go
+++ ddc/keeper/keeper.go
@@ -7,7 +7,10 @@ import (
 	authkeeper "github.com/bianjieai/ddc-go/ddc/core/auth/keeper"
 	feekeeper "github.com/bianjieai/ddc-go/ddc/core/fee/keeper"
 	tokenkeeper "github.com/bianjieai/ddc-go/ddc/core/token/keeper"
+	"github.com/bianjieai/ddc-go/ddc/export"
 )
+
+var _ export.Hook = Keeper{}
 
 // Keeper of the auth store
 type Keeper struct {
@@ -23,4 +26,84 @@ func NewKeeper(cdc codec.Codec, key sdk.StoreKey) Keeper {
 		FeeKeeper:   feekeeper.NewKeeper(cdc, key),
 		TokenKeeper: tokenkeeper.NewKeeper(cdc, key),
 	}
+}
+
+// BeforeDenomTransfer implements export.Hook
+func (k Keeper) BeforeDenomTransfer(ctx sdk.Context, protocol string, denomID string, sender sdk.AccAddress) error {
+	if err := k.AuthKeeper.BeforeDenomTransfer(ctx, protocol, denomID, sender); err != nil {
+		return err
+	}
+
+	if err := k.TokenKeeper.BeforeDenomTransfer(ctx, protocol, denomID, sender); err != nil {
+		return err
+	}
+
+	if err := k.FeeKeeper.BeforeDenomTransfer(ctx, protocol, denomID, sender); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BeforeTokenBurn implements export.Hook
+func (k Keeper) BeforeTokenBurn(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress) error {
+	if err := k.AuthKeeper.BeforeTokenBurn(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+
+	if err := k.TokenKeeper.BeforeTokenBurn(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+
+	if err := k.FeeKeeper.BeforeTokenBurn(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BeforeTokenEdit implements export.Hook
+func (k Keeper) BeforeTokenEdit(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress) error {
+	if err := k.AuthKeeper.BeforeTokenEdit(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+
+	if err := k.TokenKeeper.BeforeTokenEdit(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+
+	if err := k.FeeKeeper.BeforeTokenEdit(ctx, protocol, denomID, tokenID, sender); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BeforeTokenMint implements export.Hook
+func (k Keeper) BeforeTokenMint(ctx sdk.Context, protocol string, denomID string, sender sdk.AccAddress, receiver sdk.AccAddress) error {
+	if err := k.AuthKeeper.BeforeTokenMint(ctx, protocol, denomID, sender, receiver); err != nil {
+		return err
+	}
+
+	if err := k.TokenKeeper.BeforeTokenMint(ctx, protocol, denomID, sender, receiver); err != nil {
+		return err
+	}
+
+	if err := k.FeeKeeper.BeforeTokenMint(ctx, protocol, denomID, sender, receiver); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BeforeTokenTransfer implements export.Hook
+func (k Keeper) BeforeTokenTransfer(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress, receiver sdk.AccAddress) error {
+	if err := k.AuthKeeper.BeforeTokenTransfer(ctx, protocol, denomID, tokenID, sender, receiver); err != nil {
+		return err
+	}
+
+	if err := k.TokenKeeper.BeforeTokenTransfer(ctx, protocol, denomID, tokenID, sender, receiver); err != nil {
+		return err
+	}
+
+	if err := k.FeeKeeper.BeforeTokenTransfer(ctx, protocol, denomID, tokenID, sender, receiver); err != nil {
+		return err
+	}
+	return nil
 }
