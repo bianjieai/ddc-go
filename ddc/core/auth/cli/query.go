5bdb62f8fc5e37a621daba2607d9f72a078cb69e
Dreamer <745124335@qq.com>
2022-07-07 16:20:53 +0800

    add command

--- ddc/core/auth/cli/query.go
+++ ddc/core/auth/cli/query.go
@@ -1,9 +1,12 @@
 package cli
 
 import (
+	"github.com/bianjieai/ddc-go/ddc/core/auth"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // GetQueryCmd returns the cli query commands for the module.
@@ -17,7 +20,38 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(
-	// TODO
+		GetCmdQueryAccount(),
 	)
+	return cmd
+}
+
+// GetCmdQueryAccount implements the query account command.
+func GetCmdQueryAccount() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "account [address]",
+		Args:  cobra.ExactArgs(1),
+		Short: "query an account information",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			_, err = sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			queryClient := auth.NewQueryClient(clientCtx)
+			res, err := queryClient.Account(cmd.Context(), &auth.QueryAccountRequest{
+				Address: args[0],
+			})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
--- ddc/core/auth/keeper/query_server.go
+++ ddc/core/auth/keeper/query_server.go
@@ -3,17 +3,42 @@ package keeper
 import (
 	context "context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/bianjieai/ddc-go/ddc/core/auth"
 )
 
 var _ auth.QueryServer = Keeper{}
 
 // Account implements auth.QueryServer
-func (Keeper) Account(context.Context, *auth.QueryAccountRequest) (*auth.QueryAccountResponse, error) {
-	panic("unimplemented")
+func (k Keeper) Account(goctx context.Context, req *auth.QueryAccountRequest) (*auth.QueryAccountResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	if req.Address == "" {
+		return nil, status.Error(codes.InvalidArgument, "address cannot be empty")
+	}
+
+	_, err := sdk.AccAddressFromBech32(req.Address)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid address: %s", err.Error())
+	}
+
+	ctx := sdk.UnwrapSDKContext(goctx)
+	account, err := k.GetAccount(ctx, req.Address)
+	if err != nil {
+		return nil, err
+	}
+
+	return &auth.QueryAccountResponse{
+		AccountInfo: account,
+	}, nil
 }
 
 // Functions implements auth.QueryServer
-func (Keeper) Functions(context.Context, *auth.QueryFunctionsRequest) (*auth.QueryFunctionsResponse, error) {
+func (Keeper) Functions(goctx context.Context, req *auth.QueryFunctionsRequest) (*auth.QueryFunctionsResponse, error) {
 	panic("unimplemented")
 }
