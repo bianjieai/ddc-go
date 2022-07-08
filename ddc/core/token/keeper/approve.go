a950591657c00924a3eb3f476e82ac27fc7bf609
yuandu <taramaoriskykage@gmail.com>
2022-07-08 14:12:10 +0800

    chore: code style

--- ddc/core/token/keeper/approve.go
+++ ddc/core/token/keeper/approve.go
@@ -35,7 +35,31 @@ func (k Keeper) approve(ctx sdk.Context, denom, tokenID, operator, to string) er
 		return sdkerrors.Wrapf(token.ErrInvalidOperator, "approve operator is not owner nor approved for all")
 	}
 
-	k.saveDDCApprovals(ctx, denom, tokenID, to)
+	k.setDDCApprovals(ctx, denom, tokenID, to)
+
+	return nil
+}
+
+// implement: https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC721/DDC721.sol#L274
+func (k Keeper) setApproveForAllDDC721(ctx sdk.Context, denom, sender, operator string) error {
+
+	if !k.requireApprovalConstraintsDDC721(ctx, operator, operator) {
+		// TODO
+	}
+
+	nft, exist := k.nftKeeper.GetDenom(ctx, denom)
+	if !exist {
+		return sdkerrors.Wrapf(token.ErrNonExistentDDC, "nft denom is not existent")
+	}
+
+	// NOTE: is Creator the Owner?
+	if nft.Creator != sender {
+		return sdkerrors.Wrapf(token.ErrInvalidOwner, "sender is not the nft owner")
+	}
+
+	k.setAccountApprovalKey(ctx, denom, nft.Creator, operator)
+
+	// TODO: event
 
 	return nil
 }
@@ -65,57 +89,12 @@ func (k Keeper) setApproveForAllDDC1155(ctx sdk.Context, denom, sender, operator
 	return nil
 }
 
-// implement: https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC721/DDC721.sol#L274
-func (k Keeper) setApproveForAllDDC721(ctx sdk.Context, denom, sender, operator string) error {
-
-	if !k.requireApprovalConstraintsDDC721(ctx, operator, operator) {
-		// TODO
-	}
-
-	nft, exist := k.nftKeeper.GetDenom(ctx, denom)
-	if !exist {
-		return sdkerrors.Wrapf(token.ErrNonExistentDDC, "nft denom is not existent")
-	}
-
-	// NOTE: is Creator the Owner?
-	if nft.Creator != sender {
-		return sdkerrors.Wrapf(token.ErrInvalidOwner, "sender is not the nft owner")
-	}
-
-	k.setAccountApprovalKey(ctx, denom, nft.Creator, operator)
-
-	// TODO: event
-
-	return nil
-}
-
-// implement:
-// - https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC721/DDC721.sol#L288
-// - https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC1155/DDC1155.sol#L206
-func (k Keeper) isApprovedForAll(ctx sdk.Context, denom, owner, operator string) bool {
-	store := k.prefixStore(ctx)
-	return store.Has(accountApprovalKey(denom, owner, operator))
-}
-
-func (k Keeper) isInBlacklist(ctx sdk.Context, denom, tokenID string) bool {
-	store := k.prefixStore(ctx)
-	return store.Has(tokenBlacklistKey(denom, tokenID))
-}
-
 // implement:  https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC721/DDC721.sol#L245
 func (k Keeper) requireApprovalConstraintsDDC721(ctx sdk.Context, operator, to string) bool {
 	// TODO
-	if !k.requireSenderHasFuncPermission() {
-		// TODO
-	}
-
-	if !k.requireAvailableDDCAccount() {
-		// TODO
-	}
-
-	if !k.requireOnePlatform() {
-		// TODO
-	}
+	// if !k.requireSenderHasFuncPermission() {}
+	// if !k.requireAvailableDDCAccount() {}
+	// if !k.requireOnePlatform() {}
 
 	return true
 }
@@ -126,22 +105,28 @@ func (k Keeper) requireApprovalConstraintsDDC721(ctx sdk.Context, operator, to s
 // - https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC1155/DDC1155.sol#L621
 func (k Keeper) requireApprovalConstraintsDDC1155(ctx sdk.Context, operator string) bool {
 	// TODO
-	if !k.requireSenderHasFuncPermission() {
-		// TODO
-	}
-
-	if !k.requireAvailableDDCAccount() {
-		// TODO
-	}
-
-	if !k.requireOnePlatform() {
-		// TODO
-	}
+	// if !k.requireSenderHasFuncPermission() {}
+	// if !k.requireAvailableDDCAccount() {}
+	// if !k.requireOnePlatform() {}
 
 	return true
 }
 
-func (k Keeper) saveDDCApprovals(ctx sdk.Context, denom, tokenId, to string) {
+// implement:
+// - https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC721/DDC721.sol#L288
+// - https://github.com/bianjieai/tibc-ddc/blob/master/contracts/logic/DDC1155/DDC1155.sol#L206
+func (k Keeper) isApprovedForAll(ctx sdk.Context, denom, owner, operator string) bool {
+	store := k.prefixStore(ctx)
+	return store.Has(accountApprovalKey(denom, owner, operator))
+}
+
+// implement:
+func (k Keeper) isInBlacklist(ctx sdk.Context, denom, tokenID string) bool {
+	store := k.prefixStore(ctx)
+	return store.Has(tokenBlacklistKey(denom, tokenID))
+}
+
+func (k Keeper) setDDCApprovals(ctx sdk.Context, denom, tokenId, to string) {
 	store := k.prefixStore(ctx)
 	store.Set(ddcApprovalKey(denom, tokenId), []byte(to))
 }
