11d74d280f7d2d981cdfc0488490207ad16c4f6f
Dreamer <745124335@qq.com>
2022-07-13 16:54:09 +0800

    finish hook

--- ddc/core/fee/keeper/hook.go
+++ ddc/core/fee/keeper/hook.go
@@ -3,32 +3,63 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/bianjieai/ddc-go/ddc/core"
 	"github.com/bianjieai/ddc-go/ddc/exported"
 )
 
 var _ exported.Hook = Keeper{}
 
 // BeforeDenomTransfer implements exported.Hook
-func (Keeper) BeforeDenomTransfer(ctx sdk.Context, protocol string, denomID string, sender sdk.AccAddress) error {
-	panic("unimplemented")
+func (Keeper) BeforeDenomTransfer(ctx sdk.Context,
+	protocol string,
+	denomID string,
+	sender sdk.AccAddress,
+) error {
+	//do nothing
+	return nil
 }
 
 // BeforeTokenBurn implements exported.Hook
-func (Keeper) BeforeTokenBurn(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress) error {
-	panic("unimplemented")
+func (k Keeper) BeforeTokenBurn(ctx sdk.Context,
+	protocol string,
+	denomID string,
+	tokenID string,
+	sender sdk.AccAddress,
+) error {
+	proto := core.Protocol_value[protocol]
+	return k.pay(ctx, sender.String(), core.Protocol(proto), denomID, core.Function_EDIT)
 }
 
 // BeforeTokenEdit implements exported.Hook
-func (Keeper) BeforeTokenEdit(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress) error {
-	panic("unimplemented")
+func (k Keeper) BeforeTokenEdit(ctx sdk.Context,
+	protocol string,
+	denomID string,
+	tokenID string,
+	sender sdk.AccAddress,
+) error {
+	//do nothing
+	return nil
 }
 
 // BeforeTokenMint implements exported.Hook
-func (Keeper) BeforeTokenMint(ctx sdk.Context, protocol string, denomID string, sender sdk.AccAddress, receiver sdk.AccAddress) error {
-	panic("unimplemented")
+func (k Keeper) BeforeTokenMint(ctx sdk.Context,
+	protocol string,
+	denomID string,
+	sender sdk.AccAddress,
+	receiver sdk.AccAddress,
+) error {
+	proto := core.Protocol_value[protocol]
+	return k.pay(ctx, sender.String(), core.Protocol(proto), denomID, core.Function_MINT)
 }
 
 // BeforeTokenTransfer implements exported.Hook
-func (Keeper) BeforeTokenTransfer(ctx sdk.Context, protocol string, denomID string, tokenID string, sender sdk.AccAddress, receiver sdk.AccAddress) error {
-	panic("unimplemented")
+func (k Keeper) BeforeTokenTransfer(ctx sdk.Context,
+	protocol string,
+	denomID string,
+	tokenID string,
+	sender sdk.AccAddress,
+	receiver sdk.AccAddress,
+) error {
+	proto := core.Protocol_value[protocol]
+	return k.pay(ctx, sender.String(), core.Protocol(proto), denomID, core.Function_TRANSFER)
 }
